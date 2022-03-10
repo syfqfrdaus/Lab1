566ff55435542d156982d51dabd0a083f355ae37
syfqfrdaus <syafiqfirdaus3221@gmail.com>
2022-03-10 18:39:13 +0800

    Third Commit

--- samples/src/demo/Location.java
+++ samples/src/demo/Location.java
@@ -3,7 +3,6 @@ package demo;
 public class Location {
 	
 	private String address;
-	private int postcode;
 
 	public Location() {
 		// TODO Auto-generated constructor stub
@@ -17,13 +16,4 @@ public class Location {
 		this.address = address;
 	}
 	
-	public int getPostcode() {
-		return postcode;
-	}
-	
-	public void setPostcode(int postcode) {
-		this.postcode = postcode;
-	}
-	
-
 }
