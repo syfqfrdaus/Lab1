a510ef90b0307195dac1d82d6edbbedecd488e45
syfqfrdaus <syafiqfirdaus3221@gmail.com>
2022-03-10 18:31:16 +0800

    First Commit

---
+++ samples/src/demo/Location.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Location {
+	
+	private String address;
+
+	public Location() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
