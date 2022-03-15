b73bea874dd667f2ee54a194b04cf265af37a953
syfqfrdaus <syafiqfirdaus3221@gmail.com>
2022-03-16 01:27:08 +0800

    Comit

--- Lab1/src/Exercise7/Main.java
+++ Lab1/src/Exercise7/Main.java
@@ -1,22 +1,30 @@
 package Exercise7;
 import java.util.Scanner;
+import Exercise8.WeightList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Please Enter You Weight In KG : ");
-		double weight = input.nextDouble();
+		Scanner input = new Scanner(System.in);	
 		
 		Calculate calculate = new Calculate();
-		
-		double MarsWeight = calculate.getMars(weight);
-		
-		System.out.println("The Weight On Earth is "+weight+"Kg\n");
-		System.out.println("The Weight On Mars is :"+MarsWeight+"Kg\n");
-		
+		WeightList weightlist = new WeightList();
+			
+		double[] newArr1 = weightlist.retEarth();
+		double[] newArr2 = weightlist.retMars();
+		for (int i=0; i<5; i++)
+		{
+			System.out.println("Please Enter You Weight In KG : ");
+			double weight = input.nextDouble();	
+			double MarsWeight = calculate.getMars(weight);		
+			
+			newArr1[i] = MarsWeight;
+			newArr2[i] = weight;
+			
+			System.out.println("The Weight On Earth is "+newArr2[i]+"Kg\n");
+			System.out.println("The Weight On Mars is :"+newArr1[i]+"Kg\n");
+		}
 
 	}
 
--- Lab1/src/Exercise8/WeightList.java
+++ Lab1/src/Exercise8/WeightList.java
@@ -2,19 +2,23 @@ package Exercise8;
 import java.util.ArrayList;
 import java.util.Iterator;
 import Exercise7.Calculate;
+import Exercise7.Main;
 
 public class WeightList {
 	
-	public WeightList(){
-		Calculate x1 = new Calculate();
-		Calculate x2 = new Calculate();
-		Calculate x3 = new Calculate();
-
-		ArrayList<Calculate> weightlist = new ArrayList<Calculate>();
-
-		weightlist.add(x1);
-		weightlist.add(x2);
-		weightlist.add(x3);
-		}
-
+	double[] EarthArr = new double[10];
+	double[] MarsArr = new double[10];
+	
+	public WeightList()
+	{}
+	
+	public double[] retEarth()
+	{
+		return EarthArr.clone();
+	}
+	
+	public double[] retMars()
+	{
+		return MarsArr.clone();
+	}
 }
